a212a9f9fc313e82575bf40ea1327a342e89dbd6
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-21 21:02:01 +0530

    Login setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <main>
-      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
