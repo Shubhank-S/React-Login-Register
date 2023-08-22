b7aa689acf3695f0907fd216d63aa92bd7cbc87a
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-22 14:31:07 +0530

    Logout functionlity Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Error from "./pages/Error";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="*" element={<Error />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const useName = JSON.parse(localStorage.getItem("user"));
+  //   console.log(useName);
+  const handleLogout = () => {
+    localStorage.removeItem("loggedUser");
+    navigate("/login");
+  };
   return (
     <Box
       maxWidth={450}
@@ -18,11 +26,13 @@ function Home() {
       <Typography variant="h4" padding={3} className="heading">
         HOME
       </Typography>
+      <h1>Welcome {useName.name}</h1>
       <Button
         type="submit"
         variant="contained"
         color="secondary"
         sx={{ borderRadius: 3, marginTop: 3 }}
+        onClick={handleLogout}
       >
         LOGOUT
       </Button>
